9bcc2cb21a0ab664fbe8d7e4e2b4556f0e9a767a
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-11-01 19:40:51 +0100

    feat: Setup CLI foundation (#3)
    * feat: add CLI entry point in src/index.ts
    
    * feat: add option to specify path scan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("it works bla");
+import { program } from "commander";
+import Scanner from "./scanner.js";
+
+program
+  .name("ondesided")
+  .description("A CLI to manage your side projects.")
+  .version("0.1.0")
+  .option("-d, --dir <path>", "Directory to scan")
+  .action((options) => {
+    const scanner = new Scanner();
+    const projectDirectories = scanner.scanFolder(options.dir);
+    console.log(projectDirectories);
+  });
+
+program.parse();

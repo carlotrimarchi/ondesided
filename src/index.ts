7326cec053bfbc28e06264a559ab7591f55fd6f9
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-11-01 18:53:38 +0100

    feat: add CLI entry point in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("it works bla");
+import { program } from "commander";
+
+program
+  .name("ondesided")
+  .description("A CLI to manage your side projects.")
+  .version("0.1.0")
+  .action(() => console.log("it works"));
+
+// program.command("ondesided").action(() => console.log("Hey"));
+
+program.parse();

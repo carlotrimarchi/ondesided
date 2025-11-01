9ea64e8290f044c41803daf038ef4e6d65487e34
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-11-01 19:31:53 +0100

    feat: add option to specify path scan

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { program } from "commander";
+import Scanner from "./scanner.js";
 
 program
   .name("ondesided")
   .description("A CLI to manage your side projects.")
   .version("0.1.0")
-  .action(() => console.log("it works"));
-
-// program.command("ondesided").action(() => console.log("Hey"));
+  .option("-d, --dir <path>", "Directory to scan")
+  .action((options) => {
+    const scanner = new Scanner();
+    const projectDirectories = scanner.scanFolder(options.dir);
+    console.log(projectDirectories);
+  });
 
 program.parse();

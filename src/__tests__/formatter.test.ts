195afdae927a7c4a5b2a3cd7824ebf0ad2423ad3
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-11-02 18:11:52 +0100

    test: cover default, tsv, json case

--- src/__tests__/formatter.test.ts
+++ src/__tests__/formatter.test.ts
@@ -1,67 +1,65 @@
-import { describe, expect, it, vi, beforeEach } from "vitest";
-import { vol, fs } from "memfs";
-import Scanner from "../scanner.js";
+import { describe, expect, it } from "vitest";
+import { Dirent } from "fs";
 import formatter from "../formatter.js";
 
-vi.mock("fs", async () => {
-  const memfs = await import("memfs");
-  return { default: memfs.fs };
-});
+describe("formatter", () => {
+  const mockProjects = [
+    { name: "project1", path: "/projects" },
+    { name: "project2", path: "/projects" },
+    { name: "project3", path: "/projects" },
+  ] as Dirent[];
 
-beforeEach(() => {
-  vol.reset();
-});
+  describe("path format (default)", () => {
+    it("should return a string of paths separated by a new line", () => {
+      const result = formatter(mockProjects);
 
-const projectsDir = "/projects";
-const mockFileSystem = {
-  "project1/.git/config": "",
-  "project2/.git/config": "",
-  "project3/.git/config": "",
-  "dir1/main.js": "console.log('hello')",
-  "dir2/index.js": "console.log('world')",
-  "empty-dir/.gitkeep": "",
-  file1: "just a regular file",
-  file2: "another regular file",
-  ".DS_Store": "",
-};
+      expect(result).toBe(
+        "/projects/project1\n/projects/project2\n/projects/project3",
+      );
+    });
 
-it("should return a string of paths separated by a new line", () => {
-  vol.fromJSON(mockFileSystem, projectsDir);
-  const scanner = new Scanner();
-  const projectDirectories = scanner.scanFolder(projectsDir);
+    it("should return an empty string when no projects are found", () => {
+      const mockProjects = [] as Dirent[];
+      const result = formatter(mockProjects);
+      expect(result).toBe("");
+    });
+  });
 
-  const output = formatter(projectDirectories);
+  describe("tsv format", () => {
+    it("should return name and path separated by tabs, one per line", () => {
+      const result = formatter(mockProjects, "tsv");
+      expect(result).toBe(
+        "project1\t/projects/project1\nproject2\t/projects/project2\nproject3\t/projects/project3",
+      );
+    });
 
-  expect(output).toBe(
-    "/projects/project1\n/projects/project2\n/projects/project3",
-  );
-});
+    it("should return an empty string when no projects are found", () => {
+      const mockProjects = [] as Dirent[];
+      const result = formatter(mockProjects, "tsv");
+      expect(result).toBe("");
+    });
+  });
 
-it("should return an empty string when no projects are found", () => {
-  const mockFileSystem = {
-    "dir1/main.js": "console.log('hello')",
-    "dir2/index.js": "console.log('world')",
-    "empty-dir/.gitkeep": "",
-    file1: "just a regular file",
-    file2: "another regular file",
-    ".DS_Store": "",
-  };
+  describe("json format", () => {
+    it("should return a json array containing objects with project name and folder", () => {
+      const result = formatter(mockProjects, "json");
+      const parsed = JSON.parse(result);
 
-  vol.fromJSON(mockFileSystem, projectsDir);
-  const scanner = new Scanner();
-  const projectDirectories = scanner.scanFolder(projectsDir);
+      const expected = [
+        { name: "project1", path: "/projects/project1" },
+        { name: "project2", path: "/projects/project2" },
+        { name: "project3", path: "/projects/project3" },
+      ];
 
-  const output = formatter(projectDirectories);
-  expect(output).toBe("");
-});
+      expect(parsed).toEqual(expected);
+    });
 
-it("should return name and path separated by tabs, one per line", () => {
-  vol.fromJSON(mockFileSystem, projectsDir);
-  const scanner = new Scanner();
-  const projectDirectories = scanner.scanFolder(projectsDir);
+    it("should return an empty JSON string when no projects are found", () => {
+      const mockProjects = [] as Dirent[];
+      const result = formatter(mockProjects, "json");
+      const parsed = JSON.parse(result);
 
-  const output = formatter(projectDirectories, "tsv");
-  expect(output).toBe(
-    "project1\t/projects/project1\nproject2\t/projects/project2\nproject3\t/projects/project3",
-  );
+      expect(parsed).toEqual([]);
+    });
+  });
 });
--- src/formatter.ts
+++ src/formatter.ts
@@ -10,6 +10,15 @@ export default function formatter(
     return directories
       .map((dir) => `${dir.name}\t${dir.path}/${dir.name}`)
       .join("\n");
+  } else if (format === "json") {
+    const output = directories.map((dir) => {
+      return {
+        name: dir.name,
+        path: `${dir.path}/${dir.name}`,
+      };
+    });
+    console.log("output", output);
+    return JSON.stringify(output);
   } else {
     return directories.map((dir) => `${dir.path}/${dir.name}`).join("\n");
   }
